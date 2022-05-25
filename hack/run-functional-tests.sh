ff660d09851c5a638ec8e826da96856ca3afa278
Mario Costa <mario.costa@trustly.com>
2022-05-25 12:36:44 -0300

    Trying to fix yq cmd usage

--- hack/run-functional-tests.sh
+++ hack/run-functional-tests.sh
@@ -214,7 +214,7 @@ scope_and_run_tests() {
 
   # size the array of resources
   local resource_array_length
-  resource_array_length=$(yq 'resources' "${TESTS_MANIFEST}")
+  resource_array_length=$(yq '.resources | length' "${TESTS_MANIFEST}")
 
 
   # loop through all resources in the supplied manifest
